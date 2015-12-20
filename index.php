bb149c937c2028c0c9f1f3fe5c3c5ff7a7518b8e
llvc <llvc1986@outlook.com>
2015-12-20 03:08:42 +0100

    another

--- index.php
+++ index.php
@@ -19,18 +19,25 @@
 <h1>Register here!</h1>
 <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
 <form method="post" action="index.php" enctype="multipart/form-data" >
-      Name  <input type="text" name="name" id="name"/></br>
-      Email <input type="text" name="email" id="email"/></br>
+      Name  <input type="text" name="text" id="text"/></br>
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
     // DB connection info
     //TODO: Update the values for $host, $user, $pwd, and $db
     //using the values you retrieved earlier from the Azure Portal.
+    
+    // $host = "br-cdbr-azure-south-a.cloudapp.net:3306";
+    // $user = "b313cd7a7c53ac";
+    // $pwd = "ce8b4f97";
+    // $db = "web-mysqldb";
+
+    
     $host = "localhost";
     $user = "root";
     $pwd = "";
-    $db = "webappmysqlgroupdb";
+    $db = "web-mysqldb";
+
     // Connect to database.
     try {
         $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
@@ -42,16 +49,13 @@
     // Insert registration info
     if(!empty($_POST)) {
     try {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $date = date("Y-m-d");
+        $name = $_POST['text'];
         // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date)
-                   VALUES (?,?,?)";
+        $sql_insert = "INSERT INTO TodoItem (text, complete)
+                   VALUES (?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $name);
-        $stmt->bindValue(2, $email);
-        $stmt->bindValue(3, $date);
+        $stmt->bindValue(2, false);
         $stmt->execute();
     }
     catch(Exception $e) {
@@ -60,19 +64,15 @@
     echo "<h3>Your're registered!</h3>";
     }
     // Retrieve data
-    $sql_select = "SELECT * FROM registration_tbl";
+    $sql_select = "SELECT * FROM TodoItem";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll();
     if(count($registrants) > 0) {
         echo "<h2>People who are registered:</h2>";
         echo "<table>";
-        echo "<tr><th>Name</th>";
-        echo "<th>Email</th>";
-        echo "<th>Date</th></tr>";
+        echo "<tr><th>Text</th>";
         foreach($registrants as $registrant) {
-            echo "<tr><td>".$registrant['name']."</td>";
-            echo "<td>".$registrant['email']."</td>";
-            echo "<td>".$registrant['date']."</td></tr>";
+            echo "<tr><td>".$registrant['text']."</td>" . "</tr>";
         }
         echo "</table>";
     } else {
